ae84e673e4e2609f96b5666bd4ae89215cad0b2d
Tyler Cromwell <tyler@csh.rit.edu>
2014-01-07 23:09:12 -0500

    Added debug statements

--- strutil.c
+++ strutil.c
@@ -7,19 +7,24 @@
 #include "data-structs/vector.h"
 #include "strutil.h"
 
-#define STRING_END '\0' // Null terminator, marks end of a string.
+#ifdef STRUTIL_DEBUG
+	#include "colors.h"
+#endif
+
 #define BLANK_SPACE 32  // The ASCII value for the Space character.
+#define STRING_END '\0' // Null terminator, marks end of a string.
 
 /*
- * Gets the index of a character in a string.
+ * Gets the first index of a character in a string.
  * Argument(s):
  *   const char* string, the string being examined.
  *   const char character, the character whose position being searched for.
- * Memory Management:
- *   Nothing to worry about here.
  * Returns: the index of 'character' in 'string'
  */
 int indexOf(const char* string, const char character) {
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Searching \"%s\" for first occurence of \'%c\'\n" COLOR_RESET, string, character);
+	#endif
 	unsigned int i = 0;
 	while (string[i] != character) i++;
 	return i;
@@ -36,6 +41,9 @@ int indexOf(const char* string, const char character) {
  * Returns: an array of all the indexes that 'character' is found at.
  */
 int* indexesOf(char* string, char character, int* amount) {
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Searching \"%s\" for all occurences of \'%c\'\n" COLOR_RESET, string, character);
+	#endif
 	int buff[strlen(string)];
 	for (unsigned int i = 0; i < strlen(string); i++) {
 		if (string[i] == character) {
@@ -56,11 +64,12 @@ int* indexesOf(char* string, char character, int* amount) {
  *   char* string, the string to be modified.
  *   char old, the character being replaced.
  *   char new, the character doing the replacing.
- * Memory Management:
- *   Nothing to worry about here.
  * Returns: void.
  */
 void replaceAll(char* string, char old, char new) {
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Replacing all occurences of \'%c\' with \'%c\' in \"%s\"\n" COLOR_RESET, old, new, string);
+	#endif
 	int amount = 0;
 	int* indexes = indexesOf(string, old, &amount);
 	for (unsigned int i = 0; i < amount; i++)
@@ -72,12 +81,15 @@ void replaceAll(char* string, char old, char new) {
  * Splits up a string at a given token.
  * Argument(s):
  *   char* string, the string to be split.
- *   const char* token, the token used to split 'str' at.
+ *   const char* token, the token used to split 'string' at.
  * Memory Management:
  *   Free the array member from the returned pointer when done.
  * Returns: a resizeable array of tokens.
  */
 Vector vector_split(char* string, const char* token) {
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Spliting \"%s\" at every occurence of \"%s\"\n" COLOR_RESET, string, token);
+	#endif
 	Vector tokens = vector_init(0);
 	char* part;
 	part = strtok(string, token);
@@ -98,8 +110,11 @@ Vector vector_split(char* string, const char* token) {
  *   Free the string array when done.
  * Returns: an array of tokens.
  */
-char** split_string(char* string, char* delimiter, int* tokenAmount) {
-	char** tokens = calloc((*tokenAmount)+1, sizeof(char));
+char** split_string(char* string, const char* delimiter, int* tokenAmount) {
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Spliting \"%s\" at every occurence of \"%s\"\n" COLOR_RESET, string, delimiter);
+	#endif
+	char** tokens = calloc(1, sizeof(char*));
 	char* tok = strtok(string, delimiter);
 	for (unsigned int i = 0; tok != NULL; i++) {
 		tokens[i] = tok;
@@ -111,7 +126,7 @@ char** split_string(char* string, char* delimiter, int* tokenAmount) {
 }
 
 /*
- * Derives a substring from a given string.
+ * Derives a substring from a given string. (start <= N < end)
  * Argument(s):
  *   char* original, the string to derive the substring from.
  *   int start, the starting index.
@@ -121,10 +136,16 @@ char** split_string(char* string, char* delimiter, int* tokenAmount) {
  * Returns: the substring.
  */
 char* substring(char* original, int start, int end) {
-	char* substring = calloc(end-start+1, sizeof(char));
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Retrieving substring of \"%s\" from %d to %d\n" COLOR_RESET, original, start, end);
+	#endif
+	char* substring = calloc((end-start)+1, sizeof(char));
 	for (unsigned int i = 0; i < (end-start); i++)
 		substring[i] = original[start+i];
 	substring[end-start] = STRING_END;
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Substring of \"%s\" from %d to %d is \"%s\"\n" COLOR_RESET, original, start, end, substring);
+	#endif
 	return substring;
 }
 
@@ -137,6 +158,9 @@ char* substring(char* original, int start, int end) {
  * Returns: the trimmed version of 'original'.
  */
 char* trim(char* original) {
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: Trimming \"%s\"\n" COLOR_RESET, original);
+	#endif
 	int i = 0;
 	for (; original[i] == BLANK_SPACE; i++);
 	int start = i;
