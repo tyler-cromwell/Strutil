066127887ce9a299e3924a68232aef9c3ce2bc26
Tyler Cromwell <tyler@csh.rit.edu>
2014-04-23 14:59:38 -0400

    Moved macros into header file

--- strutil.h
+++ strutil.h
@@ -1,6 +1,23 @@
 #ifndef STRUTIL_H_
 #define STRUTIL_H_
 
+#ifdef STRUTIL_DEBUG
+	#ifndef COLOR_YELLOW
+		#define COLOR_YELLOW "\x1b[33m" // ANSI Escape Code for turning on yellow terminal text
+	#endif
+	#ifndef COLOR_RESET
+		#define COLOR_RESET  "\x1b[0m"  // ANSI Escape Code for turning off colored terminal text
+	#endif
+#endif
+
+#ifndef ASCII_SPACE
+	#define ASCII_SPACE 32 // The ASCII value for the Space character.
+#endif
+
+#ifndef ASCII_NULL
+	#define ASCII_NULL  0  // The ASCII value for the Null character.
+#endif
+
 extern int strutil_contains(char* string, char* sequence);
 extern int strutil_endsWith(char* string, char* endtok);
 extern int strutil_startsWith(char* string, char* starttok);
