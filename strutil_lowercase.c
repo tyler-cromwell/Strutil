54a2994a8a53b9cda84281dd246ee3a5c4d53230
Tyler Cromwell <tyler@csh.rit.edu>
2014-07-29 23:07:47 -0400

    Added a function to change alphabetic characters to lowercase

---
+++ strutil_lowercase.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "strutil.h"
+
+#ifdef STRUTIL_DEBUG
+	#include <stdio.h>
+#endif
+
+char* strutil_lowercase(char* string) {
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: LOWERCASE: Setting all alphabetic characters in \"%s\" to lowercase.\n" COLOR_RESET, string);
+	#endif
+
+	size_t length = strlen(string);
+	char* lower = calloc(length+1, sizeof(char));
+
+	strncpy(lower, string, length);
+
+	for (size_t i = 0; i < length; i++) {
+		if (65 <= lower[i] && lower[i] <= 90)
+			lower[i] = (char) string[i] + 32;
+	}
+
+	#ifdef STRUTIL_DEBUG
+		printf(COLOR_YELLOW "STRUTIL: LOWERCASE: \"%s\" transformed to \"%s\".\n" COLOR_RESET, string, lower);
+	#endif
+	return lower;
+}
