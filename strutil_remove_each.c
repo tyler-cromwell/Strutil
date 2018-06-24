e202d72c237f3e20faa617328cc326041b2020c5
Tyler Cromwell <tjc6185@gmail.com>
2018-06-24 14:36:46 -0400

    Fixed strutil_remove_each.c

--- strutil_remove_each.c
+++ strutil_remove_each.c
@@ -18,14 +18,22 @@
 char* strutil_remove_each(char* string, char* chars) {
     char* shortened = calloc(strlen(string) - strlen(chars) + 1, sizeof(char));
     size_t index = 0;
+    char exclude = 0;
 
     for (size_t i = 0; i < strlen(string); i++) {
         for (size_t j = 0; j < strlen(chars); j++) {
-            if (string[i] != chars[j]) {
-                shortened[index] = string[i];
-                index++;
+            if (string[i] == chars[j]) {
+                exclude = 1;
+                break;
             }
         }
+
+        if (!exclude) {
+            shortened[index] = string[i];
+            index++;
+        }
+
+        exclude = 0;
     }
 
     shortened[index] = '\0';
