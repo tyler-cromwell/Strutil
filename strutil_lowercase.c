b49b5306a11a215d48bb8651870af31e4cd7f605
Tyler Cromwell <tjc6185@gmail.com>
2018-06-24 14:55:54 -0400

    strutil_lowercase does not create a new string

--- strutil_lowercase.c
+++ strutil_lowercase.c
@@ -8,21 +8,13 @@
  * Sets alphabetic characters to lowercase.
  * Argument(s):
  *   char *string: The original string.
- * Memory Management:
- *   Free the returned pointer when done.
- * Returns: A new string with lowercase letters.
  */
-char *strutil_lowercase(char *string) {
+void strutil_lowercase(char *string) {
     size_t length = strlen(string);
-    char *lower = calloc(length+1, sizeof(char));
 
     for (size_t i = 0; i < length; i++) {
         if (string[i] >= 'A' && string[i] <= 'Z') {
-            lower[i] = (char) (string[i] + 32);
-        } else {
-            lower[i] = string[i];
+            string[i] = (char) (string[i] + 32);
         }
     }
-
-    return lower;
 }
