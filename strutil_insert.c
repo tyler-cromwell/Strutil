dc8391933696662785717fc3ad657bebd016605f
Tyler Cromwell <tjc6185@gmail.com>
2018-07-12 22:42:34 -0400

    Added an insert function

---
+++ strutil_insert.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <string.h>
+
+#include "strutil.h"
+
+
+/*
+ * Argument(s):
+ *   char *original: the string to derive the substring from.
+ *   size_t start: the starting index.
+ *   size_t end: the ending index.
+ * Returns: the substring.
+ * (Pointer must be freed)
+ */
+char * strutil_insert(char *original, char *additional, size_t index) {
+    size_t lo = strlen(original);
+    size_t la = strlen(additional);
+
+    if (index >= lo) {
+        return NULL;
+    }
+
+    char *expanded = calloc(lo + la, sizeof(char));
+
+    if (index == 0) {
+        strncpy(expanded, additional, la);
+        strncat(expanded, original, lo);
+    } else {
+        strncpy(expanded, original, index);
+        strncat(expanded, additional, la);
+        strncat(expanded, original + index, la - index);
+    }
+
+    return expanded;
+}
